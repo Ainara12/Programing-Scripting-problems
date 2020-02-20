f7b37838fdb2bdb252a0ef8a0ad2b7444a7a8909
Ainara Ruiz <you@example.com>
2020-02-20 14:45:09 +0000

    new file added for week 6

---
+++ functionpractice.py
@@ -0,0 +1,11 @@
+#Ainara Ruiz Castillo
+#I am creating my first function following steps from week 6 tutorials
+
+#This function is used to square numbers
+
+def power (x,y):
+
+
+power (2,3)
+
+    
